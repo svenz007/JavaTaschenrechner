c87e95fc9221f08d15545f835fe265dc19e53e4c
svenz007 <97756263+svenz007@users.noreply.github.com>
2022-03-20 16:33:00 +0100

    third

--- src/Mainclass.java
+++ src/Mainclass.java
@@ -1,5 +1,5 @@
 public class Mainclass {
     public static void main(String[] args) {
-System.out.println("hallo");
+
     }
 }
--- src/TestPleger.java
+++ src/TestPleger.java
@@ -1,3 +1,3 @@
 public class TestPleger {
-    system.out.print("hello");
+
 }
